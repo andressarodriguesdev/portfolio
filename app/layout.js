3baee09273de8c2e450aed0990f300702b5684bd
Andressa <andressa.rodrigues.2172@gmail.com>
2024-12-21 09:08:36 -0300

    Alterações no layout da pagina

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ import "./globals.css";
 import Head from 'next/head'
 
 export const metadata = {
-  title: "Website Portfolio",
-  description: "Portfolio Web Design",
+  title: "Andressa Rodrigues",
+  description: "Portfolio de Andressa Rodrigues",
 };
 
 export default function RootLayout({ children }) {
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
-        <link rel="icon" href="/path-to-your-favicon.ico" />  {/* Corrigido link para ícone */}
+        <link rel="icon" href="./avatar.png" />  {/* Corrigido link para ícone */}
       </Head>
       <body>{children}</body>
     </html>
